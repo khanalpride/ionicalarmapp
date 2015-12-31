e666a19b5103f1867825ccc579e49ffac15cd566
Santosh Khanal <santosh.khanal55@gmail.com>
2015-12-31 23:30:01 +0545

    Finished Front end work

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('alarmapp', ['ionic', 'alarmapp.controllers'])
+angular.module('alarmapp', ['ionic','ionic.service.core', 'alarmapp.controllers'])
 
 .config(function($ionicConfigProvider) {
   $ionicConfigProvider.scrolling.jsScrolling(true);
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,61 +5,98 @@ angular.module('alarmapp.controllers', [])
     {
       name: 'Alarm 1',
       time: '2016/01/01 22:00',
-      status: false
+      status: false,
+      snooze: false
     },
     {
       name: 'Alarm 2',
       time: '2016/01/02 22:00',
-      status: false
+      status: false,
+      snooze: false
     },
     {
       name: 'Alarm 3',
       time: '2016/01/03 22:00',
-      status: true
+      status: true,
+      snooze: false
     },
     {
       name: 'Alarm 4',
       time: '2016/01/04 22:00',
-      status: true
+      status: true,
+      snooze: false
     }
   ];
 
   $scope.currentAlarm = {};
 
-  // Create the login modal that we will use later
+  $scope.alarmOnAirNow = 0;
+
+  // Create the alarm add modal
   $ionicModal.fromTemplateUrl('templates/alarm.html', {
     scope: $scope
   }).then(function(modal) {
-    $scope.modal = modal;
+    $scope.modalAlarm = modal;
+  });
+
+   // Create the alarm modal
+  $ionicModal.fromTemplateUrl('templates/alarmShow.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.snoozeAlarm = modal;
   });
 
   $scope.edit = function(alarm) {
-    $scope.currentAlarm = {
-      name: alarm.name,
-      time: alarm.time,
-      editing: true
-    }
+    $scope.editing = true;
+    $scope.currentAlarm = alarm;
 
-    $scope.modal.show();
+    $scope.modalAlarm.show();
   };
 
   $scope.closeModal = function() {
-    $scope.modal.hide();
+    $scope.modalAlarm.hide();
+  };
+
+  $scope.alarmOnAir = function() {
+    $scope.snoozeAlarm.show();
+  };
+
+  $scope.Snooze = function(snoozeTime) {
+    $scope.SnoozeAlarmHide();
+    var alarmAfter = snoozeTime * 60 * 1000;
+    console.log('Alarm is set after ' + alarmAfter + ' Mili seconds or ' + (alarmAfter / 1000) + ' seconds');
+    setTimeout($scope.alarmOnAir, alarmAfter);
+  };
+
+  $scope.SnoozeAlarmHide = function() {
+    $scope.snoozeAlarm.hide();
   };
 
   $scope.addNew = function() {
+    $scope.editing = false;
     $scope.currentAlarm = {
-      editing: false
+      status: true
     };
 
-    $scope.modal.title = title;
-
-    $scope.modal.show();
+    $scope.modalAlarm.show();
   };
 
   $scope.saveData = function() {
-    if($scope.currentAlarm.editing) {
+    if($scope.currentAlarm.name === undefined  && $scope.currentAlarm.time === undefined ) {
+      console.log('set error in inputs');
+    }
+    else {
+      if($scope.editing == true) {
+        var alarmIndex = $scope.alarms.indexOf($scope.currentAlarm);
+        $scope.alarms[alarmIndex] = $scope.currentAlarm;
+      }
+      else {
+        $scope.alarms.push($scope.currentAlarm);
+      }
 
+      setTimeout($scope.closeModal, 400);
     }
   };
+
+  setTimeout($scope.alarmOnAir, 6000);
 });
